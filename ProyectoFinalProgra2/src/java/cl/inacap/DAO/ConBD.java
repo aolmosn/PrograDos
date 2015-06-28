5388f915aff5a23f5f042a3596a4a9b9ac00e1ba
Boris Hernández <bohndez@gmail.com>
2015-06-28 13:29:17 -0300

    Con ConBD

--- ProyectoFinalProgra2/src/java/cl/inacap/DAO/ConBD.java
+++ ProyectoFinalProgra2/src/java/cl/inacap/DAO/ConBD.java
@@ -5,10 +5,25 @@
  */
 package cl.inacap.DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author andre_000
  */
 public class ConBD {
+    String user = "progrados";
+    String pass = "123456";
+    String url="jdbc:oracle:thin:@localhost:1521:XE";
     
+    public Connection getConexion() throws ClassNotFoundException, SQLException {
+        //Se registra el driver.
+        Class.forName("oracle.jdbc.OracleDriver");
+        
+        //Le pongo las variables como parámetros.
+        Connection con = DriverManager.getConnection(url, user, pass);
+        return con;
+    }
 }
